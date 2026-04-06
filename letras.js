2cb012037ae8d61ca6585ba29155917cb67d167b
Professor Rafael Selvagio <professor.rafael.selvagio@gmail.com>
2026-04-05 21:48:18 -0300

    Refina interface de silabas e completa alfabeto

--- letras.js
+++ letras.js
@@ -8,9 +8,9 @@ const syllablesButton = document.querySelector("#syllables-btn");
 
 const letters = [
   "A", "B", "C", "D", "E", "F", "G",
-  "H", "I", "J", "L", "M", "N", "O",
-  "P", "Q", "R", "S", "T", "U", "V",
-  "X", "Z",
+  "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U",
+  "V", "W", "X", "Y", "Z",
 ];
 
 const syllableRows = [
